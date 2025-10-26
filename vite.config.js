65525928e69b35f204d6fa577e6ec9f57e4c1387
ZhouZhengyang <642278479@qq.com>
2025-10-26 15:59:50 +0800

    修改打包配置

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './', // 关键：设置为相对路径，避免打包后资源路径错误
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
